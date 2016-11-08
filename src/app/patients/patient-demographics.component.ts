7638b8dbcfbf7299fd7b806e874dc185612e97eb
Blake Mumford <blake.mumford@indoctrinate.com.au>
2016-11-09 00:33:19 +1100

    Changed icons, upgrade to angular 2.1.1

--- src/app/patients/patient-demographics.component.ts
+++ src/app/patients/patient-demographics.component.ts
@@ -1,20 +1,21 @@
 import {Component, OnInit} from "@angular/core";
 import {PatientManagerService} from "./patient-manager.service";
+import Patient = fhir.Patient;
 @Component({
   selector: 'patient-demographics',
   template: require('./patient-demographics.component.html')
 })
 
 export class PatientDemographicsComponent implements OnInit {
-  givenName: string;
-  familyName: string;
+
+  patient: Patient;
 
   constructor(private patientManagerService: PatientManagerService) {
+
   }
 
 
   ngOnInit(): void {
-    this.givenName = this.patientManagerService.currentPatient.name[0].given[0];
-    this.familyName = this.patientManagerService.currentPatient.name[0].family[0];
+    this.patient = this.patientManagerService.currentPatient;
   }
 }
--- src/app/patients/patient.module.ts
+++ src/app/patients/patient.module.ts
@@ -7,9 +7,10 @@ import {patientRouting} from "./patient.routing";
 import {PatientHomeComponent} from "./patient-home.component";
 import {OpenPatientsComponent} from "./open-patients.component";
 import {PatientSummaryComponent} from "./patient-summary.component";
+import {PatientDemographicsComponent} from "./patient-demographics.component";
 
 @NgModule({
-  declarations: [PatientShellComponent, PatientBannerComponent, PatientHomeComponent, OpenPatientsComponent, PatientSummaryComponent],
+  declarations: [PatientShellComponent, PatientBannerComponent, PatientHomeComponent, OpenPatientsComponent, PatientSummaryComponent, PatientDemographicsComponent],
   exports: [PatientShellComponent, PatientBannerComponent],
   imports: [CommonModule, FormsModule, patientRouting],
   providers: []
